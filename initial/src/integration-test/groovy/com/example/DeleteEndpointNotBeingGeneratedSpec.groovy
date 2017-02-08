324ab4fdd0455a9a18095d8b3ddc2ac8c85ceddb
sdelamo <sergio.delamo@softamo.com>
2017-02-08 11:14:19 +0100

    Test delete endpoint is not generated

---
+++ initial/src/integration-test/groovy/com/example/DeleteEndpointNotBeingGeneratedSpec.groovy
@@ -0,0 +1,32 @@
+package com.example
+
+import grails.plugins.rest.client.RestBuilder
+import grails.test.mixin.integration.Integration
+import grails.transaction.Rollback
+import spock.lang.Specification
+
+@Integration
+@Rollback
+class DeleteEndpointNotBeingGeneratedSpec extends Specification {
+
+    def "test Grails does not generate endpoints for update/create/delete operations at Domain classes annotated with @Resource(readOnly=true)"() {
+        given:
+        RestBuilder rest = new RestBuilder()
+
+        when:
+        def resp = rest.delete("http://localhost:${serverPort}/api/order/1") {
+            header("Accept", "application/json")
+        }
+
+        then:
+        resp.status == 405
+
+        when:
+        resp = rest.get("http://localhost:${serverPort}/api/order/1") {
+            header("Accept", "application/json")
+        }
+
+        then:
+        resp.status == 200
+    }
+}
