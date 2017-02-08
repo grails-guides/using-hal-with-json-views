f12d76510753591cf21e704df621b474b2339d81
sdelamo <sergio.delamo@softamo.com>
2017-02-08 11:45:12 +0100

    Remove unnecessary snippet

--- complete/grails-app/controllers/UrlMappings.groovy
+++ complete/grails-app/controllers/UrlMappings.groovy
@@ -2,20 +2,12 @@ class UrlMappings {
 
     static mappings = {
 
-
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
-
         delete "/$controller/$id(.$format)?"(action:"delete")
         get "/$controller(.$format)?"(action:"index")
         get "/$controller/$id(.$format)?"(action:"show")
         post "/$controller(.$format)?"(action:"save")
         put "/$controller/$id(.$format)?"(action:"update")
         patch "/$controller/$id(.$format)?"(action:"patch")
-
 
         "/api/products"(resources:"product")
 
